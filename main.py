96e3beaee43e105c09e2300551ac264aa9d7cdb7
Saad Aldoaij <54961548+Saadaldoaij@users.noreply.github.com>
2022-06-25 15:37:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import csv
-
-
 print("\n")
-
 print("""
 
 ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
@@ -10,18 +7,18 @@ print("""
 ██ █████▄▄▄▀▀███ █ █████▄▄▄▀▀██ ▄▄▄█ ▀▀ ██ ▀▀▄██ █████ ▄▄ ████ ▄▄▄██ █ █ ██ █▀▀██ ███ █ █ ██ ▄▄▄
 ██ ▀▀▄██ ▀▀▀ ███▄▀▄█████ ▀▀▀ ██ ▀▀▀█ ██ ██ ██ ██ ▀▀▄██ ██ ████ ▀▀▀██ ██▄ ██ ▀▀▄█▀ ▀██ ██▄ ██ ▀▀▀
 ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-
 """)
 print("\n\n\n")
+
 try:
     while True:
         path = input("Enter the PATH ")
         # Removing "" from the path
         path = path.replace('"', '')
-        spred = open(path, "r")
+        spred = open(path, "r", encoding='utf-8')
         reader = csv.reader(spred)
         x = 0
-        print("\n\n\n\n\n\n")
+        print("\n\n"*3)
 
         # read the entity name :
         z = int(0)
@@ -45,12 +42,13 @@ try:
         keyWord = input("Enter a keyword: \n")
         # Searcher
 
-        x = 0
+        x = 1
         for i in reader:
             x += 1
 
             if i[search].find(keyWord) > -1:
                 print("----------- \n \nI found one! \nAnd it's in Line:", x)
 
+
 except:
-    print("")
+    print("Somthing wrong occur! ")
